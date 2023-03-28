f5c643e878fe6ccef8ab3e604ab07d57a247a5c9
Nick Bohm <nickbohm@Nicks-MacBook-Air.local>
2023-03-28 15:22:08 -0400

    changed readme

--- Algo_Solver/main.py
+++ Algo_Solver/main.py
@@ -20,35 +20,6 @@ graph = {
 
 def main():
     """main function which includes my functions."""
-    # testable function 1
-    print(sorting.radix_sort([15, 3, 222, 11, 8]))
-
-    # testable function 2
-    print(searching.selection_algo([15, 3, 222, 11, 8], 1))
-
-    # testable function 3
-    print(graphs.bfs(graph, 'A'))
-
-    # testable function 4
-    print(graphs.dfs(graph, 'A'))
-
-    # testable function 5
-    print(sorting.quick_sort([15, 3, 222, 11, 8]))
-
-    # testable function 6
-    print(searching.binary_search([1, 10, 20, 50, 100, 200], 50))
-
-    # testable function 7
-    print(sorting.bubble_sort([15, 3, 222, 11, 8]))
-
-    # testable function 8
-    print(sorting.bucket_sort([15, 3, 222, 11, 8]))
-
-    # testable function 9
-    print(sorting.insertion_sort([15, 3, 222, 11, 8]))
-
-    # testable function 10
-    print(sorting.merge_sort([15, 3, 222, 11, 8]))
 
 
 if __name__ == "__main__":
