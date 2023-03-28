8cb28644d79b8946b2b759ce3eb0312fd95879bc
Nick Bohm <nickbohm@Nicks-MacBook-Air.local>
2023-03-28 15:26:16 -0400

    last changes

--- Algo_Solver/main.py
+++ Algo_Solver/main.py
@@ -3,20 +3,6 @@ __author__ = "Nicholas Bohm"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
-import graphs
-import searching
-import sorting
-
-
-graph = {
-    'A': set(['B', 'C']),
-    'B': set(['A', 'D', 'E']),
-    'C': set(['A', 'F']),
-    'D': set(['B']),
-    'E': set(['B', 'F']),
-    'F': set(['C', 'E']),
-}
-
 
 def main():
     """main function which includes my functions."""
